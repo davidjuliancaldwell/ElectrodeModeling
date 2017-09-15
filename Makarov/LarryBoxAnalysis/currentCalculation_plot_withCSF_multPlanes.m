40e96bfa1126430449f0d4fbc52539fbd0588bda
David Caldwell <davidjuliancaldwell@gmail.com>
2017-09-15 14:34:30 +0200

    changes for plotting with multiple planes

--- Makarov/LarryBoxAnalysis/currentCalculation_plot_withCSF_multPlanes.m
+++ Makarov/LarryBoxAnalysis/currentCalculation_plot_withCSF_multPlanes.m
@@ -2,7 +2,7 @@ function [] = currentCalculation_plot_withCSF_multPlanes(input_struct)
 
 % extract current from structure
 
-for ind = 1:3
+for ind = 1:2
 current = input_struct{ind}.Current;
 
 % calculate the magnitude of the current vector at each point
@@ -29,8 +29,65 @@ figure
 plot(keys,summed_level,'o','markersize',5)
 %xlim([min(keys) max(keys)])
 xlim([min(keys) 10])
-
+vline(0)
+ylabel('Ratio of current')
+xlabel('Depth in mm')
 end
+
+% do the xz plane 
+ind = 3;
+current = input_struct{ind}.Current;
+
+% calculate the magnitude of the current vector at each point
+netCurrent_pt = power((power(current(:,1),2)+power(current(:,2),2)+power(current(:,3),2)),0.5);
+total_sum = sum(netCurrent_pt);
+potential_pt = input_struct{ind}.Potential;
+
+% generate profile 
+points_total = input_struct{ind}.Points;
+z_dimension = points_total(:,3)*-1000;
+x_dimension = points_total(:,1)*1000;
+
+keys_x = unique(x_dimension)';
+keys_z = unique(z_dimension)';
+
+dims_x = range(keys_x);
+dims_z = range(keys_z); 
+
+
+[X,Z] = meshgrid(keys_x,keys_z);
+data = reshape(netCurrent_pt,[length(keys_x) length(keys_z)])';
+% figure
+% mesh(X,Z,data)
+% title('Magnitude of Current in Space - xz plane')
+% 
+% figure
+% contour(X,Z,data)
+% title('Magnitude of Current in Space - xz plane')
+
+figure
+pcolor(X,Z,data)
+shading interp
+colorbar()
+title('Magnitude of Current in Space - xz plane')
+
+
+data = reshape(potential_pt,[length(keys_x) length(keys_z)])';
+% figure
+% mesh(X,Z,data)
+% title('Electric Potential in Space - xz plane')
+% 
+% figure
+% contour(X,Z,data)
+% title('Electric Potential in Space - xz plane')
+
+figure
+pcolor(X,Z,data)
+shading interp
+colorbar()
+title('Magnitude of Potential in Space - xz plane')
+
+
 
 % do the imagesc ones to look @ 2D planes 
 for ind = 4:5
@@ -44,8 +101,8 @@ total_sum = sum(netCurrent_pt);
 
 % generate profile 
 points_total = input_struct{ind}.Points;
-x_dimension = points_total(:,1)*-1000;
-y_dimension = points_total(:,2)*-1000;
+x_dimension = points_total(:,1)*1000;
+y_dimension = points_total(:,2)*1000;
 
 keys_x = unique(x_dimension)';
 keys_y = unique(y_dimension)';
@@ -56,16 +113,35 @@ dims_y = range(keys_y);
 
 [X,Y] = meshgrid(keys_x,keys_y);
 data = reshape(netCurrent_pt,[length(keys_x) length(keys_y)]);
+% figure
+% mesh(X,Y,data)
+% title('Magnitude of Current in Space - xy plane')
+% 
+% figure
+% contour(X,Y,data)
+% title('Magnitude of Current in Space - xy plane')
+
 figure
-mesh(X,Y,data)
-figure
-contour(X,Y,data)
+pcolor(X,Y,data)
+shading interp
+colorbar()
+title('Magnitude of Current in Space - xy plane')
 
 data = reshape(potential_pt,[length(keys_x) length(keys_y)]);
+% figure
+% mesh(X,Y,data)
+% title('Electric Potential in Space - xy plane')
+% 
+% figure
+% contour(X,Y,data)
+% title('Electric Potential in Space - xy plane')
+
 figure
-mesh(X,Y,data)
-figure
-contour(X,Y,data)
+pcolor(X,Y,data)
+shading interp
+colorbar()
+title('Electric Potential in Space - xy plane')
+
 
 end
 
