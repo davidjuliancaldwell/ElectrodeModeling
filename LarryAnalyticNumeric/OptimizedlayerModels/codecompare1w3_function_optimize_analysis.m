fdb62c5a373f3f9bdc793ea00658c33aafe8687f
David Caldwell <djcald@cs.washington.edu>
2018-08-29 12:17:02 -0700

    new analysis function to compare the 1 and 3 layer models, make plots of errors, as well as analyze distance bin fits

--- LarryAnalyticNumeric/OptimizedlayerModels/codecompare1w3_function_optimize_analysis.m
+++ LarryAnalyticNumeric/OptimizedlayerModels/codecompare1w3_function_optimize_analysis.m
@@ -1,193 +1,8 @@
-% %% initialize workspace
-% %clear all; close all;clc
-% load('all12.mat')
-% 
-% % define matrices to iterate over
-% dataTotal_8x8 = 4.*[m0b5a2e m702d24 m7dbdec m9ab7ab mc91479 md5cd55 mecb43e];
-% dataTotal_8x4 = 4.*[m2012(1,:)' m2804(1,:)' m2318(1,:)' m2219(1,:)' m2120(1,:)'];
-% sidVec = {'0b5a2e','702d24','7dbdec','9ab7ab','c91479','d5cd55','ecb43e','m2012','m2804','m2318','m2219','m2120'};
-% %sidVec = {'0b5a2e','702d24','7dbdec','9ab7ab','c91479','d5cd55','ecb43e'};
-% 
-% currentMat = [0.00175 0.00075 0.0035 0.00075 0.003 0.0025 0.00175 0.0005 0.0005 0.0005 0.0005 0.0005] ;
-% 
-% % GLOBAL FIT
-% stimChansVec = {22 30; 13 14; 11 12; 59 60; 55 56; 54 62; 56 64; 12 20; 4 28; 18 23; 19 22; 21 20};
-% 
-% % LOCAL FIT
-% %stimChansVec = {1:40; 1:33; 1:32; 40:64; [1,33:64]; 39:64; 25:64};
-% 
-% jp_vec = [3 2 2 8 7 7 7 3 4 3 3 3];
-% kp_vec = [6 5 3 3 7 6 8 4 4 7 6 5];
-% jm_vec = [4 2 2 8 7 8 8 2 1 3 3 3];
-% km_vec = [6 6 4 4 8 6 8 4 4 2 3 4];
-prepare_data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% script to analyze the results of the one and 3 layer optimization on all electrodes at once
+% David.J.Caldwell 8.29.2018
 
-% define colors for lines
-
-color1 = [27,201,127]/256;
-color2 = [190,174,212]/256;
-color3 = [ 253,192,134]/256;
-
-% perform optimization for the 1 layer case
-
-a=0.00115;
-R=0.00115;
-d=0.0035;
-sigma = 0.05; % this defines for huber loss the transition from squared
-% to linear loss behavior
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% optimization for 1 layer
-rhoA_vec=[2:0.01:6];
-%offset_vec=[-3e-3:1e-5:3e-3];
-%offset_vec = [0,1];
-offset_vec = [0];
-cost_vec_1layer = zeros(length(sidVec),length(rhoA_vec),length(offset_vec));
-
-subject_min_rhoA_vec = zeros(length(sidVec),1);
-subject_min_offset1l_vec = zeros(length(sidVec),1);
-%%
-
-% loop through subjects
-for i = 1:length(sidVec)
-    %for i = 1
-    
-    % select particular values for constants
-    i0 = currentMat(i);
-    sid = sidVec(i);
-    stimChans = [(stimChansVec{:})];
-    jp = jp_vec(i);
-    kp = kp_vec(i);
-    jm = jm_vec(i);
-    km = km_vec(i);
-    
-    % perform 1d optimization
-    j = 1;
-    for rhoA = rhoA_vec
-        % extract measured data and calculate theoretical ones
-        k = 1;
-        for offset = offset_vec
-            if i <= 8 % 8x8 cases
-                dataMeas = dataTotal_8x8(:,i);
-                [l1] = computePotentials_8x8_l1(jp,kp,jm,km,rhoA,i0,stimChans,offset);
-                % c91479 was flipped l1 l3
-                if strcmp(sid,'c91479')
-                    l1 = -l1;
-                end
-                
-            else % 8x4 case
-                dataMeas = dataTotal_8x4(:,i-7);
-                [l1] = computePotentials_8x4_l1(jp,kp,jm,km,rhoA,i0,stimChans,offset);
-            end
-            
-            % use a huber loss functiin
-            %[h_loss,huber_all] = huber_loss_electrodeModel(dataMeas,l1,sigma);
-            
-            % use sum sqaured
-            h_loss = nansum((dataMeas' - l1).^2);
-            
-            cost_vec_1layer(i,j,k) = h_loss;
-            fprintf(['complete for subject ' num2str(i) ' rhoA = ' num2str(rhoA) ' offset = ' num2str(offset) ' \n ']);
-            k = k + 1;
-        end
-        j = j + 1;
-    end
-    
-end
-
-%[~,index_min] = min(cost_vec_1layer,[],2);
-%subject_min_rho = rhoA_vec(index_min);
-%%
-for i = 1:length(sidVec)
-    %for i = 1
-    cost_vec_subj = squeeze(cost_vec_1layer(i,:,:));
-    
-    [value, index] = min(cost_vec_subj(:));
-    %[ind1,ind2] = ind2sub(size(cost_vec_subj),index);
-    [ind1] = ind2sub(size(cost_vec_subj),index);
-    
-    subject_min_rhoA_vec(i) = rhoA_vec(ind1);
-    %subject_min_offset1l_vec(i) = offset_vec(ind2);
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% 3 layer
-% optimization for 3 layer
-rho1_vec = [0.55];
-rho2_vec= [1.5:0.05:7.5];
-rho3_vec = [1.5:0.05:7.5];
-height_vec = [0:0.0001:0.002];
-%offset_vec=[-3e-3:1e-5:3e-3];
-offset_vec = 0;
-sigma = 0.05; % sigma for huber loss
-cost_vec_3layer = zeros(length(sidVec),length(height_vec),length(rho1_vec),length(rho2_vec),length(rho3_vec),length(offset_vec));
-
-%subject_min_rho1_vec = zeros(length(sidVec),1);
-%subject_min_rho2_vec = zeros(length(sidVec),1);
-%subject_min_rho3_vec = zeros(length(sidVec),1);
-%subject_min_offset3l_vec = zeros(length(sidVec),1);
-
-for i = 1:length(sidVec)
-    
-    % select particular values for constants
-    i0 = currentMat(i);
-    sid = sidVec(i);
-    stimChans = [(stimChansVec{:})];
-    jp = jp_vec(i);
-    kp = kp_vec(i);
-    jm = jm_vec(i);
-    km = km_vec(i);
-    
-    h = 1;
-    for h1 = height_vec
-        % perform 3d optimization
-        j = 1;
-        for rho1 = rho1_vec
-            k = 1;
-            for rho2 = rho2_vec
-                l = 1;
-                for rho3 = rho3_vec
-                    m = 1;
-                    for offset = offset_vec
-                        [alpha,beta,eh1,eh2,ed,step,scale] = defineConstants(i0,a,R,rho1,rho2,rho3,d,h1);
-                        
-                        if i <= 8 % 8x8 cases
-                            dataMeas = dataTotal_8x8(:,i);
-                            [l3] = computePotentials_8x8_l3(jp,kp,jm,km,alpha,beta,eh1,eh2,step,ed,scale,a,stimChans,offset);
-                            % c91479 was flipped l1 l3
-                            if strcmp(sid,'c91479')
-                                l3 = -l3;
-                            end
-                            
-                        else % 8x4 case
-                            dataMeas = dataTotal_8x4(:,i-7);
-                            [l3] = computePotentials_8x4_l3(jp,kp,jm,km,alpha,beta,eh1,eh2,step,ed,scale,a,stimChans,offset);
-                        end
-                        
-                        % use huber loss
-                        % [h_loss,huber_all] = huber_loss_electrodeModel(dataMeas,l3,sigma);
-                        
-                        % use mean square loss
-                        h_loss = nansum((dataMeas' - l3).^2);
-                        
-                        cost_vec_3layer(i,h,j,k,l,m) = h_loss;
-                        m = m + 1;
-                        fprintf(['complete for subject ' num2str(i) ' height = ' num2str(h1) ' rho1 = ' num2str(rho1) ' rho2 = ' num2str(rho2) ' rho3 = ' num2str(rho3) ' offset = ' num2str(offset) ' \n' ]);
-                    end
-                    l = l + 1;
-                end
-                k = k + 1;
-            end
-            j = j + 1;
-        end
-        h = h + 1;
-    end
-end
-
-save('8_25_2018_3layer_vals.mat')
-
+load('optimized_values_rescaled_v2_wider_range')
+saveFigBool = false;
 %%
 for i = 1:length(sidVec)
     for ii = 1:length(height_vec)
@@ -205,20 +20,7 @@ for i = 1:length(sidVec)
 end
 
 %%
-% for i = 1:length(sidVec)
-%     cost_vec_subj = squeeze(cost_vec_3layer(i,:,:,:,:,:));
-%
-%     [value, index] = min(cost_vec_subj(:));
-%     [ind1,ind2,ind3] = ind2sub(size(cost_vec_subj),index);
-%
-%     subject_min_height_vec(i) = height_vec(ind1);
-%
-%     % subject_min_rho1_vec(i) = rho1_vec(ind1);
-%     subject_min_rho2_vec(i) = rho2_vec(ind2);
-%     subject_min_rho3_vec(i) = rho3_vec(ind3);
-%     %  subject_min_offset3l_vec(i) = offset_vec(ind4);
-% end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 %% overall resistivity plot with subplots
 % 8.13.2018
@@ -226,7 +28,7 @@ saveFigBool = true;
 
 figure;
 
-for i = 1:8
+for i = 1:7
     subplot(2,4,i)
     
     plot(1e3*height_vec,subject_min_rho2_vec(i,:),'o-','linewidth',2)
@@ -263,7 +65,7 @@ saveFigBool = true;
 
 figure;
 
-for i = 1:8
+for i = 1:7
     subplot(2,4,i)
     
     plot(1e3*height_vec,1./subject_min_rho2_vec(i,:),'o-','linewidth',2)
@@ -348,6 +150,51 @@ end
 
 return
 
+%% plot trajectory of MSE as function of CSF thickness
+figure
+for i = 1:7
+        cost_vec_subj = squeeze(cost_vec_3layer(i,:,:,:,:,:)); 
+        cost_vec_subj = reshape(cost_vec_subj,length(height_vec),[]);
+        subplot(2,4,i)
+        plot(1e3*height_vec,squeeze(min(cost_vec_subj,[],2)),'linewidth',2)
+    title(['Subject ' num2str(i)])
+            
+            h1 = hline(min(cost_vec_1layer(i,:)),'k','one layer point electrode');
+    h1.LineWidth = 2;
+    set(gca,'fontsize',14)
+    grid on
+    grid minor
+
+end
+xlabel('csf thickness (mm)')
+ylabel('squared error')
+set(gca,'fontsize',14)
+
+subtitle('Error by subject as a function of presumed CSF thickness ')
+
+%% now find global minimum for each subject
+        % subject_min_rho1_vec(i) = rho1_vec(ind1);
+        subject_min_h1_vec_global = [];
+        subject_min_rho1_vec_global(i) = [];
+        subject_min_rho2_vec_global(i) = [];
+        subject_min_rho3_vec_global(i) = [];
+        %  subject_min_offset3l_vec(i) = offset_vec(ind4);
+for i = 1:7
+        cost_vec_subj = cost_vec_3layer(i,:,:,:,:,:);
+        
+        [value, index] = min(cost_vec_subj(:));
+        [ind1,ind2,ind3,ind4,ind5] = ind2sub(size(cost_vec_subj),index);
+        
+        
+        % subject_min_rho1_vec(i) = rho1_vec(ind1);
+        subject_min_h1_vec_global(i) = height_vec(ind2);
+        subject_min_rho1_vec_global(i) = rho1_vec(ind3);
+        subject_min_rho2_vec_global(i) = rho2_vec(ind4);
+        subject_min_rho3_vec_global(i) = rho3_vec(ind5);
+        %  subject_min_offset3l_vec(i) = offset_vec(ind4);
+
+end
+
 %% plotting
 % plot
 
@@ -366,18 +213,18 @@ for i = 1:length(sidVec)
     % select particular values for constants
     i0 = currentMat(i);
     sid = sidVec(i);
-    stimChans = stimChansVec{i};
+    stimChans =  [(stimChansVec{:})];
     jp = jp_vec(i);
     kp = kp_vec(i);
     jm = jm_vec(i);
     km = km_vec(i);
-    rho1 = subject_min_rho1_vec(i);
-    rho2 = subject_min_rho2_vec(i);
-    rho3 = subject_min_rho3_vec(i);
-    offset_1l = subject_min_offset1l_vec(i);
-    offset_3l = subject_min_offset3l_vec(i);
+    rho1 = subject_min_rho1_vec_global(i);
+    rho2 = subject_min_rho2_vec_global(i);
+    rho3 = subject_min_rho3_vec_global(i);
+    offset_1l = 0;
+    offset_3l = 0;
     rhoA = subject_min_rhoA_vec(i);
-    h1 = subject_min_h1_vec(i);
+    h1 = subject_min_h1_vec_global(i);
     % perform 1d optimization
     [alpha,beta,eh1,eh2,ed,step,scale] = defineConstants(i0,a,R,rho1,rho2,rho3,d,h1);
     
