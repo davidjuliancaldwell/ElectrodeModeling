d95f4250cf3f513e8ff8b8324d4cf95ca4d9e850
David Caldwell <davidjuliancaldwell@gmail.com>
2019-02-27 22:17:37 -0800

    two-point measurements

--- manuscript/extraction_analysis_pipeline/voltage_monitor_analysis/Analyze_Stim_Curves/first7_analyze_monitor.m
+++ manuscript/extraction_analysis_pipeline/voltage_monitor_analysis/Analyze_Stim_Curves/first7_analyze_monitor.m
@@ -1,7 +1,7 @@
 %%
 close all;clear all;clc
 Z_Constants_Resistivity
-currentMatVec = [0.00175 0.00075 0.0035 0.00075 0.003 0.0025 0.00175]';
+currentMatVec = [0.00175 0.00075 0.0035 0.00075 0.003 0.0025 0.00175];
 
 plotIt = 0;
 savePlot = 0;
@@ -24,7 +24,7 @@ for ii = 1:length(SIDS)
     vJumpAvgMat = [vJumpAvgMat vJumpAvg];
     
 end
-
+%%
 figure
 plot(vJumpMat)
 xlabel('Trial')
@@ -32,23 +32,43 @@ ylabel('Voltage (V)')
 set(gca,'fontsize',14)
 title('Concatenated Jump Voltages for all Subjects')
 
+
 figure
 hold on
 for ii = 1:length(vJumpCell)
     plot(vJumpCell{ii})
 end
-legend()
+legend(num2str([1:7]'));
+CT = cbrewer('qual','dark2',7);
+colormap(CT)
 xlabel('Trial')
 ylabel('Voltage (V)')
-set(gca,'fontsize',14)
+set(gca,'fontsize',18)
 title('Concatenated Jump Voltages for all Subjects')
 
-
-dividedVI = vJumpAvgMat/currentMatVec;
-twoPointR = (2*0.115*dividedVI);
-
 figure
+correction = 0.98;
+dividedVI = vJumpAvgMat./currentMatVec;
+twoPointR = (2*.00115*dividedVI/(correction));
+
+subplot(1,3,1)
+plot(vJumpAvgMat)
+xlabel('Subject')
+ylabel('Voltage (V)')
+set(gca,'fontsize',14)
+title('Mean Jump Voltages')
+
+subplot(1,3,2)
+plot(dividedVI)
+xlabel('Subject')
+ylabel('Resistance (ohms)')
+set(gca,'fontsize',14)
+title('Jump Voltage/Applied Current')
+
+subplot(1,3,3)
 plot(twoPointR)
 xlabel('Subject')
-ylabel('Resistivity')
+title('Resistivities')
+ylabel('Resistivity (ohm-m)')
+set(gca,'fontsize',14)
 
