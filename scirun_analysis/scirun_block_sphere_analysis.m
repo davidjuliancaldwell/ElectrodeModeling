659a70932c35b6362aee398e11d15d9c03a1ff23
David Caldwell <davidjuliancaldwell@gmail.com>
2019-03-11 15:33:39 -0700

    grid of points on sphere, interpolate voltages, fit by bin

--- scirun_analysis/scirun_block_sphere_analysis.m
+++ scirun_analysis/scirun_block_sphere_analysis.m
@@ -75,7 +75,7 @@ minC = min([sphereV1;sphereV2]);
 maxC = max([sphereV1;sphereV2]);
 figure
 ax(1) =subplot(1,2,1);
- scatter3(sphereGeo1(:,1),sphereGeo1(:,2),sphereGeo1(:,3),25,sphereV1,'filled');
+scatter3(sphereGeo1(:,1),sphereGeo1(:,2),sphereGeo1(:,3),25,sphereV1,'filled');
 title('2 layer with 1 conductivity')
 caxis([minC,maxC])
 
@@ -94,12 +94,12 @@ minC = min([sphereV1_1;sphereV1;sphereV2]);
 maxC = max([sphereV1_1;sphereV1;sphereV2]);
 figure
 ax(1) =subplot(1,3,1);
- scatter3(sphereGeo1_1(:,1),sphereGeo1_1(:,2),sphereGeo1_1(:,3),25,sphereV1_1,'filled');
+scatter3(sphereGeo1_1(:,1),sphereGeo1_1(:,2),sphereGeo1_1(:,3),25,sphereV1_1,'filled');
 title('1 layer with 1 conductivity')
 caxis([minC,maxC])
 
 ax(2) =subplot(1,3,2);
- scatter3(sphereGeo1(:,1),sphereGeo1(:,2),sphereGeo1(:,3),25,sphereV1,'filled');
+scatter3(sphereGeo1(:,1),sphereGeo1(:,2),sphereGeo1(:,3),25,sphereV1,'filled');
 title('2 layer with 1 conductivity')
 caxis([minC,maxC])
 
@@ -113,280 +113,131 @@ setappdata(gcf, 'StoreTheLink', Link);
 colorbar()
 %%
 figure
-zSliceInt = 0;
-zIndex = blockGeo(:,3)==zSliceInt;
-imagesc(blockGeo(zIndex,1),blockGeo(zIndex,2),blockV(zIndex))
-
-%%
-% set light scale for visualization
-lightScale = 0.8;
-figure;
-set(gca, 'ZDir','reverse')
-p = patch(isosurface(blockGeo(:,1),blockGeo(:,2),blockGeo(:,3),blockV,1.25));
-% get normals to the surface - this helps with lighting
-isonormals(blockGeo(:,1),blockGeo(:,2),blockGeo(:,3),blockV,p)
-
-% check MATLAB version for compatability
-if verLessThan('matlab','8.5')
-    set(p,'FaceColor',[0.8 0.8 0.8]);
-    set(p,'EdgeColor','none');
-else
-    p.FaceColor = [0.8 0.8 0.8];
-    p.EdgeColor = 'none';
-    p.FaceAlpha = 0.7;
+zSliceVec = unique(blockGeo(:,3));
+zSliceVec = 100;
+for zSliceInt = zSliceVec'
+    zSliceInt;
+    zIndex = blockGeo(:,3)==zSliceInt;
+    scatter(blockGeo(zIndex,1),blockGeo(zIndex,2),[],blockV(zIndex),'filled')
 end
-view(3);
-%axis tight
-box off
-axis off
-%%
-% daspect should take care of the 1 mm, 1mm, 0.9 mm scales for visualizing
-% the x, y, and z
-daspect([1 1 0.9])
-view(90,90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,-90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-view(-90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-
 
 %%
-
-%Plot CSF Gray and White matter for LL slices
-%x increases from front (anterior) to back (posterior), dx=1 mm
-%y increases from top of head (dorsal) to bottom (basal), dy=1 mm
-% z increases from left to the right. Z is slice number, dz=0.9 mm
-% xy is MR slice, [256x256]
-% 4-25-2018 - David J. Caldwell
-
-clear all;clc
-load LL_CSF.mat;
-load LL_GrayMatter.mat;
-load LL_WhiteMatter.mat;
-
-% permute the order of x and y to make plotting easier
-colOrder = [3 2 1];
-CSF(:,colOrder) = flipud(CSF);
-GrayMatter(:,colOrder) = flipud(GrayMatter);
-WhiteMatter(:,colOrder) = flipud(WhiteMatter);
-
-%%%%% first we will reshape it
-jVec = [34:158]; % this iterates through the slices
-x = [1:256];
-y= [1:256];
-z = [1:256];
-
-% make a meshgrid to visualize the cube
-[X,Y,Z] = meshgrid(x,y,z);
-
-% make individual  matrices
-CSFReshape = zeros(size(X));
-GrayMatterReshape = zeros(size(Y));
-WhiteMatterReshape = zeros(size(Z));
-BrainReshape = zeros(size(X));
-
-% iterate through the j vector
-for j=jVec
-    
-    % CSF
-    % take a slice
-    SliceNumber=j;
-    
-    % take z dimension of CSF
-    J1=CSF(:,3);
-    J2=find(J1==SliceNumber);
-    CSFSlice = CSF(J2,:);
-    
-    %convert to linear indices for indexing
-    linearInd = sub2ind([256,256,256],CSFSlice(:,1),CSFSlice(:,2),CSFSlice(:,3));
-    CSFReshape(linearInd) = 1;
-    % assign CSF values of 1
-    BrainReshape(linearInd) = 1;
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % GRAY MATTER
-    J1=GrayMatter(:,3);
-    J2=find(J1==SliceNumber);
-    GrayMatterSlice = (GrayMatter(J2,:));
-    linearInd = sub2ind([256,256,256],GrayMatterSlice(:,1),GrayMatterSlice(:,2),GrayMatterSlice(:,3));
-    
-    GrayMatterReshape(linearInd) = 1;
-    BrainReshape(linearInd) = 3;
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % WHITE MATTER
-    J1=WhiteMatter(:,3);
-    J2=find(J1==SliceNumber);
-    WhiteMatterSlice = (WhiteMatter(J2,:));
-    linearInd = sub2ind([256,256,256],WhiteMatterSlice(:,1),WhiteMatterSlice(:,2),WhiteMatterSlice(:,3));
-    
-    WhiteMatterReshape(linearInd) = 1;
-    BrainReshape(linearInd) = 5;
-    
-end;
-
-%
-%BrainReshape = permute(BrainReshape,[3 1 2]);
-
-%% Now plot the Cortex highlighting either the gray or white or CSF
-% Highlight Gray Matter
-% set light scale for visualization
-lightScale = 0.8;
-figure;
-set(gca, 'ZDir','reverse')
-p = patch(isosurface(X,Y,Z,BrainReshape,1.25));
-% get normals to the surface - this helps with lighting
-isonormals(x,y,z,BrainReshape,p)
-
-% check MATLAB version for compatability
-if verLessThan('matlab','8.5')
-    set(p,'FaceColor',[0.8 0.8 0.8]);
-    set(p,'EdgeColor','none');
-else
-    p.FaceColor = [0.8 0.8 0.8];
-    p.EdgeColor = 'none';
-    p.FaceAlpha = 0.7;
-end
-view(3);
-%axis tight
-box off
-axis off
-
-% daspect should take care of the 1 mm, 1mm, 0.9 mm scales for visualizing
-% the x, y, and z
-daspect([1 1 0.9])
-view(90,90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,-90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-view(-90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-
-% lighting additions
-lighting gouraud
-material([.3 .8 .1 10 1]);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Highlight White Matter
+centerB = [0 0 100];
+cutOffLow = 99.5;
+cutOffHigh = 100;
+blockIndex = (blockGeo(:,3)<=cutOffHigh & blockGeo(:,3)>=cutOffLow);
+geoB = blockGeo(blockIndex,:);
+vB = blockV(blockIndex);
 figure
-set(gca, 'ZDir','reverse')
-p = patch(isosurface(X,Y,Z,BrainReshape,3));
-isonormals(x,y,z,BrainReshape,p)
-if verLessThan('matlab','8.5')
-    set(p,'FaceColor',[0.8 0.8 0.8]);
-    set(p,'EdgeColor','none');
-else
-    p.FaceColor = [0.8 0.8 0.8];
-    p.EdgeColor = 'none';
-    p.FaceAlpha = 0.7;
-    
+scatter3(geoB(:,1),geoB(:,2),geoB(:,3),25,log(vB),'filled')
+%% define sample points of interest for x y grid
+[gridX,gridY] = meshgrid([-35:10:35],[-40:10:30]);
+pointsGrid = [gridX(:) gridY(:)];
+pointsGrid(:,3) = repmat(100,length(pointsGrid),1);
+figure
+scatter3(blockGeo(:,1),blockGeo(:,2),blockGeo(:,3),25,(blockV),'filled')
+colorbar()
+title('Block Linear Voltage')
+hold on
+scatter3(gridX(:),gridY(:),repmat(100,length(gridX(:)),1),50,'k','filled')
+
+%% define anonymous functions to allow for conversion to mercator, and subsequent rotation
+vRot = @(v,theta,k)(v*cos(theta) + cross(k,v)*sin(theta) + k*(dot(k,v))*(1-cos(theta)));
+long = @(x,R)(x/R);
+lat = @(y,R)(2.*atan(exp(y./R)) - pi/2);
+invX = @(R,lat,long)(R.*cos(lat).*cos(long));
+invY = @(R,lat,long)(R.*cos(lat).*sin(long));
+invZ = @(R,lat,long)(R.*sin(lat));
+
+%% convert x y grid points to mercator, and then rotate to be on top of the sphere
+R = 60;
+unitVecRotate = [0 1 0];
+rotateAngle = -pi/2;
+longitude = long(pointsGrid(:,1),R);
+latitude = lat(pointsGrid(:,2),R);
+
+xOrig = invX(R,longitude,latitude);
+yOrig = invY(R,longitude,latitude);
+zOrig = invZ(R,longitude,latitude);
+
+vecOrig = [xOrig,yOrig,zOrig];
+
+for index = 1:size(vecOrig,1)
+    vecNew(index,:) = vRot(vecOrig(index,:),rotateAngle,unitVecRotate);
 end
-view(3);
-%axis tight
-axis off
-colormap(gray(50))
-box off
-axis off
-daspect([1 1 0.9])
-view(90,90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,-90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-view(-90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-
-lighting gouraud
-material([.3 .8 .1 10 1]);
-
-
-%% Now plot the cortex and a slice through it
-
-% ui box for input
-prompt = {'X limit?','Y limit?','Z limit?'};
-dlg_title = 'Subvolumes';
-num_lines = 1;
-defaultans = {'[NaN NaN]','[NaN NaN]','[55 NaN]'};
-answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
-
-xLim = str2num(answer{1});
-yLim = str2num(answer{2});
-zLim = str2num(answer{3});
-
-% get limits of the volume we are interested in
-limits = [xLim(1) xLim(2) yLim(1) yLim(2) zLim(1) zLim(2)];
-
-% extract a subset of the volume data
-[x, y, z, D] = subvolume(BrainReshape, limits);
-
-% isosurface for the outside of the volume
-[fo,vo] = isosurface(x,y,z,D,1.7);
-
-% isocaps for the end caps of the volume
-[fe,ve,ce] = isocaps(x,y,z,D,1.7);
 
 figure
-set(gca, 'ZDir','reverse')
+scatter3(xNew,yNew,zNew)
+hold on
+scatter3(vecNew(:,1),vecNew(:,2),vecNew(:,3))
 
-% draw the outside of the volume
-hiso = patch('Faces', fo, 'Vertices', vo);
+figure
+scatter3(sphereGeo1_1(:,1),sphereGeo1_1(:,2),sphereGeo1_1(:,3),25,sphereV1_1,'filled');
+hold on
+scatter3(vecNew(:,1),vecNew(:,2),vecNew(:,3),50,'k','filled')
 
-% draw the end caps of the volume
-hcap = patch('Faces', fe, 'Vertices', ve, ...
-    'FaceVertexCData', ce);
+%% scattered interpolation to get idea of voltage values at arbitrary points
 
-% check MATLAB version for compatability
+Fblock = scatteredInterpolant(blockGeo(:,1),blockGeo(:,2),blockGeo(:,3),blockV);
+Fsphere1 = scatteredInterpolant(sphereGeo1(:,1),sphereGeo1(:,2),sphereGeo1(:,3),sphereV1);
+%Fblock.Method = 'nearest';
+%Fsphere1.Method = 'nearest';
+blockValuesInt = Fblock(pointsGrid(:,1),pointsGrid(:,2),pointsGrid(:,3));
+sphereValuesInt = Fsphere1(vecNew(:,1),vecNew(:,2),vecNew(:,3));
+%%
+figure
+subplot(1,2,1)
+scatter3(pointsGrid(:,1),pointsGrid(:,2),pointsGrid(:,3),25,log(blockValuesInt),'filled');
+subplot(1,2,2)
+scatter3(vecNew(:,1),vecNew(:,2),vecNew(:,3),25,log(sphereValuesInt),'filled');
 
-if verLessThan('matlab','8.5')
-    set(hiso,'FaceColor',[0.7 0.7 0.7]);
-    set(hiso,'EdgeColor','none');
-    set(hcap,'FaceColor','interp');
-    set(hcap,'EdgeColor','none');
-else
-    hiso.FaceColor = [0.7 0.7 0.7];
-    %hiso.FaceAlpha = 0.7;
-    hiso.EdgeColor = 'none';
-    
-    hcap.FaceColor = 'interp';
-    hcap.EdgeColor = 'none';
-    
-end
+%%
+bins = 10*(repmat([1:8],2,1)+[0;1])';
+binCenter = mean(bins,2);
+rhoA = 1;
+i0 = 1e-3;
+%%
+stimChans = [29 37];
+centerSpace = [0,0,100];
+distances = vecnorm((pointsGrid-repmat(centerSpace,length(pointsGrid(:,1)),1)),2,2);
 
-colormap(gray(50))
-box off
-axis off
-daspect([1 1 0.9])
-view(90,90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,-90);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]*.3);
-view(90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
-view(-90,0);
-l = camlight('headlight');
-set(l,'color',[lightScale lightScale lightScale]);
+blockValuesInt(stimChans) = nan;
+l1 = compute_1layer_theory_coords(pointsGrid,stimChans);
+scaleA=(i0*rhoA)/(2*pi);
+l1 = scaleA*l1;
 
-lighting gouraud
-material([.3 .8 .1 10 1]);
+[rhoAoutput,MSE,subjectResiduals,offset,bestVals] = distance_selection_MSE_bins_fitlm(blockValuesInt,l1,bins,distances);
 
+tempStruct = struct;
+
+tempStruct.bestVals = bestVals;
+tempStruct.MSE = MSE;
+tempStruct.rhoAcalc = rhoAoutput;
+tempStruct.offset = offset;
+
+fprintf(['rhoA = ' num2str(rhoAoutput) ' offset = ' num2str(offset) ' \n ']);
+
+
+%%
+stimChans = [37 29];
+
+centerSpace = [0,0,60];
+distances = vecnorm((vecNew-repmat(centerSpace,length(vecNew(:,1)),1)),2,2);
+
+sphereValuesInt(stimChans) = nan;
+
+l1 = compute_1layer_theory_coords_spherical(vecNew,stimChans);
+scaleA=(i0*rhoA)/(4*pi);
+l1 = scaleA*l1;
+
+[rhoAoutput,MSE,subjectResiduals,offset,bestVals] = distance_selection_MSE_bins_fitlm(sphereValuesInt,l1,bins,distances);
+
+tempStruct = struct;
+
+tempStruct.bestVals = bestVals;
+tempStruct.MSE = MSE;
+tempStruct.rhoAcalc = rhoAoutput;
+tempStruct.offset = offset;
+
+fprintf(['rhoA = ' num2str(rhoAoutput) ' offset = ' num2str(offset) ' \n ']);
 
